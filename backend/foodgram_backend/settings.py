6687873f54e35b3bd15c1d3f99509c3b7f8c0f4e
Илья Поташев <potaschev.ilya@yandex.ru>
2024-10-07 21:26:43 +0300

    Created recipes app

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -16,7 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig',
+    'recipes.apps.RecipesConfig',
 ]
 
 MIDDLEWARE = [
