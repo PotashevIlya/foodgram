713e1bb93e5d91b79aeb31b9b6cce5f4ab262103
Илья Поташев <potaschev.ilya@yandex.ru>
2024-10-21 21:05:36 +0300

    Added tags csv-file

---
+++ backend/recipes/management/commands/fill_tags_from_csv.py
@@ -0,0 +1,19 @@
+from csv import DictReader
+
+from django.core.management.base import BaseCommand
+
+from ...models import Tag
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        with open(
+            'data/tags.csv', 'r', encoding='utf-8'
+        ) as csvfile:
+            reader = DictReader(csvfile)
+            for row in reader:
+                ingredient = Tag(
+                    name=row['name'],
+                    slug=row['slug']
+                )
+                ingredient.save()
