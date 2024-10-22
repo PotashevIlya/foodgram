553ee992c4461fcfefb1f62e3da402ae64c8d087
Илья Поташев <potaschev.ilya@yandex.ru>
2024-10-22 16:07:28 +0300

    Added static pages

--- frontend/src/pages/technologies/index.js
+++ frontend/src/pages/technologies/index.js
@@ -30,6 +30,9 @@ const Technologies = () => {
               <li className={styles.textItem}>
                 Djoser
               </li>
+              <li className={styles.textItem}>
+                Docker
+              </li>
             </ul>
           </div>
         </div>
