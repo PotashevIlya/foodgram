ba14b81c5e21486749db7905ea5ba30319ab18b4
Илья Поташев <potaschev.ilya@yandex.ru>
2024-10-23 22:51:27 +0300

    Changed short-url logic

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -52,7 +52,7 @@ def get_full_url(short_url):
     return recipe.full_url
 
 
-def redirection(short_url):
+def redirection(request, short_url):
     try:
         full_url = get_full_url(short_url)
         return redirect(full_url)
